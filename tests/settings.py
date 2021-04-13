f6e06cb52d31a5c4e1e719c009df3f2bd8a8d835
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-04-13 10:04:11 +0200

    FIAE-164: Also added channels and channels_presence to the installed apps for the test settings

--- tests/settings.py
+++ tests/settings.py
@@ -6,24 +6,13 @@ SECRET_KEY = '96a40240ed25433cb8ff8ce819bf710b'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'django_wishlist.db',
+        'NAME': 'planning_poker.db',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
 }
-
-
-MIDDLEWARE_CLASSES = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'wishlist.middleware.WishlistMiddleware',
-]
-
-
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -34,7 +23,15 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'channels',
+    'channels_presence',
     'planning_poker',
 )
 
 ROOT_URLCONF = 'planning_poker.urls'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+    },
+}
