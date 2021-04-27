13747bd1535dab7362085d44d9bfaa58ae9dded8
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-04-27 13:38:22 +0200

    the migrations will now be included in the package

--- example/settings.py
+++ example/settings.py
@@ -42,6 +42,32 @@ INSTALLED_APPS = (
     'planning_poker',
 )
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    }
+]                                                                                                   
+
 ROOT_URLCONF = 'planning_poker.urls'
 
 # Absolute path to the directory static files should be collected to.
