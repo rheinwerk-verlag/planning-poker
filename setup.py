f2154037a82486e4816abfe7ebb765d3f0f22d6b
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-05-04 09:35:05 +0200

    FIAE-164: Added more supported python version to the setup.py

--- setup.py
+++ setup.py
@@ -69,6 +69,9 @@ Private :: Do Not Upload
 Development Status :: 2 - Pre-Alpha
 Programming Language :: Python
 Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Framework :: Django
 Framework :: Django :: 3.0
 Intended Audience :: Developers
