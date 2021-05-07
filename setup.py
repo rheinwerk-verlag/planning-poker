222af8f16a40985c8dc3252ea7e394e46671a88f
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-05-07 08:04:26 +0200

    FIAE-164: removed unnecessary requirements

--- setup.py
+++ setup.py
@@ -86,10 +86,8 @@ install_requires = [
     'channels>=2.4.0',
     'channels_redis>=2.4.2',
     'Django>=3.0.3',
-    'django-auth-ldap>=2.1.1',
     'django-channels-presence>=1.0.0',
-    'jira>=2.0.0',
-    'psycopg2>=2.8.4'
+    'jira>=2.0.0'
 ]
 
 tests_require = [
