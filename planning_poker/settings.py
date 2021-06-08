0b46956565db09fcedd4f32ae58779e99b1da737
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-06-08 14:03:03 +0200

    FIAE-172: Added a brief documentation for the required `CHANNELS_LAYERS` setting

---
+++ planning_poker/settings.py
@@ -0,0 +1,3 @@
+# PLACEHOLDER UNTIL A PROPER DOCUMENTATION IS WRITTEN
+# You should add a channel layer backend to your settings.
+# Refer to https://channels.readthedocs.io/en/stable/topics/channel_layers.html.
