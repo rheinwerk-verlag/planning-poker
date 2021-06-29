1b9f3af4427a1c96247f354179bbf32cd3295592
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-06-29 08:40:52 +0200

    FIAE-172: Moved the internationalisation urls to urls of the planning poker app.

--- planning_poker/urls.py
+++ planning_poker/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import include, path
+from django.views.i18n import JavaScriptCatalog
 
 from . import views
 
@@ -6,4 +7,8 @@ app_name = 'planning_poker'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.PokerSessionView.as_view(), name='poker_session'),
+    path('jsi18n/',
+         JavaScriptCatalog.as_view(packages=['planning_poker']),
+         name='javascript-catalog'),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
