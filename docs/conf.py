71c305b4095bc8bddcb98ae3298b70643eb95cb0
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-07-15 12:12:36 +0200

    FIAE-174: Removed Markdown from the `source_suffix` list.

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosectionlabel', 'sphinx.ext.i
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
