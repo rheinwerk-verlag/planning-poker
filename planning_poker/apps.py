a1dc9d7d500f796a0c420cb169a5ee3e5ec6dc5b
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-08-11 13:59:53 +0200

    Added the `default_auto_field` attributes to the application configs.

--- planning_poker/apps.py
+++ planning_poker/apps.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class PlanningPokerConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'planning_poker'
     verbose_name = _('Planning Poker')
 
@@ -12,5 +13,6 @@ class PlanningPokerConfig(AppConfig):
 
 
 class ChannelsPresenceConfig(RoomsConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'channels_presence'
     verbose_name = _('Channels Presence')
