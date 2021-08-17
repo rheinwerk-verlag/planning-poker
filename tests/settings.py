1aaa52447ea8c73118b433cec7953c3dbcd4a9d6
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-08-17 19:18:23 +0200

    Fixed tests.

--- tests/settings.py
+++ tests/settings.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
+# Django settings for example project.
+import os
 
-# CHANGE THIS!
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 SECRET_KEY = '96a40240ed25433cb8ff8ce819bf710b'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'planning_poker/templates'),
+)
+
+ASGI_APPLICATION = 'example.asgi.application'
 
 DATABASES = {
     'default': {
@@ -14,11 +26,12 @@ DATABASES = {
     }
 }
 
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.humanize',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
@@ -28,15 +41,60 @@ INSTALLED_APPS = (
     'planning_poker',
 )
 
-ROOT_URLCONF = 'planning_poker.urls'
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
-WSGI_APPLICATION = 'example.wsgi.application'
-ASGI_APPLICATION = 'example.routing.application'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    }
+]
 
+ROOT_URLCONF = 'example.urls'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+# Please note, that the in-memory layer should not be used in production.
+# Instead install and use the channels-redis layers backend.
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer'
-    },
+    }
 }
 
-POKER_SESSION_END_REDIRECT_URL = 'index'
+LOGIN_URL = 'admin:login'
+LOGOUT_URL = 'admin:logout'
