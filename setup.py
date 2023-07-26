91e6e229ac40abed1939ea49a0520905fe5cf953
Henning Kage <henning.kage@gmail.com>
2023-07-26 15:43:56 +0200

    Removed deprecated Python 3.6 version from the tests

--- setup.py
+++ setup.py
@@ -76,7 +76,6 @@ License :: OSI Approved :: BSD License
 Operating System :: OS Independent
 Programming Language :: JavaScript
 Programming Language :: Python
-Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
